2f8414205254300c4d8dbcbb8d7cd52a05d0074d
Ashlee Foureyes <ashleefoureyes@gmail.com>
2017-08-08 23:20:43 -0400

    display data edit #1

--- load_data.py
+++ load_data.py
@@ -1,43 +1,98 @@
-import datetime as dt
-import time
+from bokeh.plotting import figure, output_file, show, ColumnDataSource
+from bkcharts import Scatter, output_file, show, Horizon, Line, TimeSeries
 import pandas as pd
-import pandas_datareader.data as web
-import bokeh
+from pandas_datareader import data, wb
 
+# define start and end dates
+start_date = '2016-06-26'
+end_date = '2016-12-31'
+
+# load data from sources
+ford = data.DataReader("NYSE:F", "google", start_date, end_date)
+generalmotors = data.DataReader("NYSE:GM", "google", start_date, end_date)
+tesla = data.DataReader("TSLA", "google", start_date, end_date)
 
 '''
-Tesla Stock Information Load
+Close Prices
+'''
+stocks = pd.DataFrame({
+    "NYSE:F": ford["Close"],
+    "TSLA": tesla["Close"],
+    "NYSE:GM": generalmotors["Close"]})
+
+stocks.head()
+stocks.reset_index(inplace=True)
+
+# p = Scatter(stocks, x='Date', y='TSLA', title='TSLA Close Price', xlabel = 'Date', ylabel='Close Price')
+
+p = Line(stocks, x='Date')
+output_file("scatter.html")
+
+show(p)
+
+'''
+Volume, Change in Volume
 '''
 
-#set keys
-tsla_open = 'Open'
-tsla_high = 'High'
-tsla_low = 'Low'
-tsla_close = 'Close'
-tsla_volume = 'Volume'
+stock_volume = pd.DataFrame({
+    "NYSE:F": ford["Volume"],
+    "TSLA": tesla["Volume"],
+    "NYSE:GM": generalmotors["Volume"]})
 
-#setting start date, end date as today's date
-start = dt.datetime(2015, 1, 1)
-end = time.strftime("%x")
+stock_volume.head()
+stock_volume.reset_index(inplace=True)
 
-#load data from google finance
+# values for volumechange function
 
-df = web.DataReader('TSLA', 'google', start, end)
+F_volume = stock_volume['NYSE:F'].tolist()
+F_vol_change = []
 
+TSLA_volume = stock_volume['TSLA'].tolist()
+TSLA_vol_change = []
 
-#load dates from Tesla Data
-dates = []
-for i in range (len(df)):
-    date_i = str(df.index[i])
-    date_i = date_i[0:10]
-    dates.append(date_i)
+GM_volume = stock_volume['NYSE:GM'].tolist()
+GM_vol_change = []
 
 
-df['dates'] = dates
+# creates list of volume changes
+
+def volume_change(x, k):
+    for i in range(0, ((len(x)) - 1)):
+        hold = x[i + 1]
+        diff = (hold - x[i])
+        k.append(diff)
+        i + 1
+    return k
 
 
+print(volume_change(F_volume, F_vol_change))
+print(volume_change(TSLA_volume, TSLA_vol_change))
+print(volume_change(GM_volume, GM_vol_change))
 
-dataFrame = pd.DataFrame(df)
+# correlation heat map .. createbetter
+correlated = stocks.corr()
+
+corrMatrix = correlated.as_matrix()
+
+N = 3
+factors = ['Tesla', 'Ford', 'GM']
+x = []
+y = []
+colors = []
+for i in range(N):
+    for j in range(N):
+        x.append(factors[j])
+        y.append(factors[i])
+        cor = corrMatrix[i, j]
+        rgb = (int(abs(cor) * 255), 0, int((1 - abs(cor)) * 255))
+        colors.append('#%02x%02x%02x' % rgb)
+
+p2 = figure(x_range=factors, y_range=factors)
+
+p2.rect(x, y, color=colors, width=1, height=1)
+output_file("heat.html")
+show(p2)
+
 
 '''
 oil prices
